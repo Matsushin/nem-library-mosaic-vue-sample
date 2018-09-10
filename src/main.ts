d85b4d2fd04e5ca97f7360a3ec826effd3ec5530
matsushin11 <matsu1983@gmail.com>
2018-09-10 23:23:06 +0900

    nem-libraryでアカウント情報を取得できるように

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import Vuetify from 'vuetify';
+import { NEMLibrary, NetworkTypes } from "nem-library";
 
 Vue.config.productionTip = false;
-Vue.use(Vuetify)
+Vue.use(Vuetify);
+NEMLibrary.bootstrap(NetworkTypes.TEST_NET);
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <h2>アカウント情報の取得</h2>
+    <v-text-field 
+      label="アドレス"
+      v-model="address"
+      :counter="40"
+      arequired
+      placeholder="例. NBHWRG6STRXL2FGLEEB2UOUCBAQ27OSGDTO44UFC"
+    ></v-text-field>
+    <v-flex>
+      <v-btn color="blue" class="white-text" @click="getAccount()">アカウント情報を取得する</v-btn>
+    </v-flex>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { AccountHttp, Address } from 'nem-library';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Component({
@@ -14,5 +25,15 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  private address: string = '';
+
+  public async getAccount() {
+    const accountHttp = new AccountHttp();
+    accountHttp.getFromAddress(new Address(this.address)).subscribe(accountInfoWithMetaData => {
+      const balance = accountInfoWithMetaData.balance.balance / 1000;
+      console.log(balance);
+    });
+  }
+}
 </script>
