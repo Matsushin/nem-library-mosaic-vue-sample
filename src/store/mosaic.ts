ff64acff6d2e47003648d93c993536780944a3b0
matsushin11 <matsu1983@gmail.com>
2018-09-12 20:53:40 +0900

    モザイクを送金できるように

--- src/store/mosaic.ts
+++ src/store/mosaic.ts
@@ -1,18 +1,33 @@
-import { AccountHttp, Address, AccountOwnedMosaicsService, MosaicHttp } from 'nem-library';
+import { AccountHttp, Address, AccountOwnedMosaicsService, MosaicHttp, MosaicId, Account, TimeWindow, TransferTransaction, TransactionHttp, EmptyMessage } from 'nem-library';
+import { Observable } from 'rxjs/Rx';
 
 export default {
     namespaced: true,
     state: {
-        mosaicLists: []
+        mosaicLists: [],
+        message: '',
+        error: '',
     },
     getters:  {
         mosaicList (state):Array<any> {
             return state.mosaicLists
-        }
+        },
+        message (state):string {
+            return state.message
+        },
+        error (state):string {
+            return state.error
+        },
     },
     mutations: {
         setMosaics (state, mosaics) {
             state.mosaicLists = mosaics
+        },
+        setMessage (state, message) {
+            state.message = message
+        },
+        setError (state, error) {
+            state.error = error
         }
     },
     actions: {
@@ -22,6 +37,33 @@ export default {
                 console.log(mosaics);
                 commit('setMosaics', mosaics)
             });
+        },
+        sendMosaic({ commit }, payload) {
+            const account = Account.createWithPrivateKey(payload.privateKey)
+            const mosaicNames = payload.mosaicName.split(':')
+            Observable.from([
+                { mosaic: new MosaicId(mosaicNames[0], mosaicNames[1]), quantity: payload.amount }
+            ]).flatMap(_ => new MosaicHttp().getMosaicTransferableWithAmount(_.mosaic, _.quantity))
+            .toArray()
+            .map(mosaics => TransferTransaction.createWithMosaics(
+                TimeWindow.createWithDeadline(),
+                new Address(payload.toAddress),
+                mosaics,
+                EmptyMessage
+                )
+            )
+            .map(transaction => account.signTransaction(transaction))
+            .flatMap(signedTransaction => new TransactionHttp().announceTransaction(signedTransaction))
+            .subscribe(
+                value => {
+                    commit('setMessage', value.message)
+                    console.log( "リクエスト結果：\n" + value.message);
+                },
+                err => {
+                    commit('setError', err.toString())
+                    console.log( "失敗：\n" + err.toString());
+                }
+            )
         }
     }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,6 +18,49 @@
       <div v-for="(item, index) in mosaicList" :key="index">
           <div>{{ item.mosaicId.namespaceId }}:{{ item.mosaicId.name }} {{ item.amount }}</div>
       </div>
+      <v-text-field 
+        label="送金先アドレス"
+        v-model="toAddress"
+        :counter="40"
+        arequired
+        placeholder="例. NBHWRG6STRXL2FGLEEB2UOUCBAQ27OSGDTO44UFC"
+      ></v-text-field>
+      <v-text-field 
+        label="秘密鍵"
+        v-model="privateKey"
+        :counter="64"
+        arequired
+      ></v-text-field>
+      <v-text-field 
+        label="モザイク"
+        v-model="mosaicName"
+        arequired
+        placeholder="matsushin:coin"
+      ></v-text-field>
+      <v-text-field 
+        label="送金量"
+        v-model="amount"
+        arequired
+      ></v-text-field>
+      <v-flex>
+        <v-btn color="blue" class="white-text" @click="sendMosaic()">モザイクを送金する</v-btn>
+      </v-flex>
+      <div v-if="message != ''">
+        <v-alert
+          :value="true"
+          type="success"
+        >
+          成功：{{message}}
+        </v-alert>
+      </div>
+      <div v-if="error != ''">
+        <v-alert
+          :value="true"
+          type="error"
+        >
+          失敗：{{error}}
+        </v-alert>
+      </div>
     </div>
   </div>
 </template>
@@ -33,14 +76,28 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
     HelloWorld,
   },
   computed: {
-    ...mapGetters({ mosaicList: 'mosaic/mosaicList' })
+    ...mapGetters({ mosaicList: 'mosaic/mosaicList', error: 'mosaic/error', message: 'mosaic/message' })
   },
 })
 export default class Home extends Vue {
   private address: string = '';
+  private toAddress: string = '';
+  private privateKey: string = '';
+  private mosaicName: string = '';
+  private amount: number = 0;
 
   public async getAccount() {
     await this.$store.dispatch('mosaic/getMosaics', this.address);
+  }
+
+  public async sendMosaic() {
+    const payload = {
+      toAddress: this.toAddress,
+      privateKey: this.privateKey,
+      mosaicName: this.mosaicName,
+      amount: this.amount
+    }
+    await this.$store.dispatch('mosaic/sendMosaic', payload);
   }
 }
 </script>
